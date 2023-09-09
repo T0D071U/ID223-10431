43722f9a52739946d9a364b25630e7b59071bb32
Timothy Deahl <116044697+T0D071U@users.noreply.github.com>
2023-09-09 16:15:20 -0600

    Finished the guess the number game.

--- chapters/chapter2/chooseARandomNumber.cpp
+++ chapters/chapter2/chooseARandomNumber.cpp
@@ -6,6 +6,24 @@
 #include <ctime>
 
 int main() {
+    std::srand(time(0));
+    const int randNumber((std::rand() % 100)+1);
 
+    int playerGuess(0);
+
+    while(playerGuess != randNumber){
+        std::cout << "Guess the number: ";
+
+        std::cin >> playerGuess;
+
+        if (playerGuess > randNumber){
+            std::cout << "Try to guess lower!\n\n";
+        } else if (playerGuess < randNumber){
+            std::cout << "Try to guess higher!\n\n";
+        }
+    }
+
+    std::cout << "Good job! The random number was " << randNumber << "!\n";
     return 0;
 }
+
